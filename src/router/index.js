c8a934cead6a842ccc5801eaccbd1a92b2eaaf7b
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2018-05-15 19:40:31 +0400

    Delete HelloWorld component and redirect `/` to `/dashboard`

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Dashboard from '@/components/Dashboard/Dashboard'
 import Calendar from '@/components/Calendar/Calendar'
 
@@ -22,7 +21,7 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      redirect: 'dashboard'
     }
   ]
 })
